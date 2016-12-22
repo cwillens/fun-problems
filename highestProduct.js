c00690f3ace397d9029381856da1c33ea97c07e2
Charlotte Willens <charwillens@gmail.com>
2016-12-21 16:08:42 -0800

    implemented highestProduct

--- highestProduct.js
+++ highestProduct.js
@@ -1,8 +1,16 @@
 var highestProduct = function(integerArray) {
   //return highest product of 3 of integers
-  var newArray = integerArray.slice().sort(function(a, b) {
-    return (a-b);
-  });
-  return Math.max(newArray[newArray.length - 1] * newArray[newArray.length - 2] * newArray[newArray.length - 3],
-    newArray[0] * newArray[1] * newArray[newArray.length - 1]);
+  var highest = Math.max(integerArray[0], integerArray[1]);
+  var lowest = Math.min(integerArray[0], integerArray[1]);
+  var highest2 = integerArray[0] * integerArray[1];
+  var lowest2 = integerArray[0] * integerArray[1];
+  var highest3 = null;
+  for (var i = 2; i < integerArray.length; i++) {
+    highest3 = Math.max(highest2 * integerArray[i], lowest2 * integerArray[i], highest3);
+    lowest2 = Math.min(lowest2, lowest * integerArray[i], highest * integerArray[i]);
+    highest2 = Math.max(highest2, highest * integerArray[i], lowest * integerArray[i]);
+    lowest = Math.min(lowest, integerArray[i]);
+    highest = Math.max(highest, integerArray[i]);
+  }
+  return highest3;
 }
